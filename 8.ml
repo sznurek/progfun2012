f8ce78c756964cb24b3adaf5f3a2bb0fa271d1a5
Łukasz Dąbek <sznurek@gmail.com>
2012-12-12 18:22:30 +0100

    List 8 - regexp.

--- 8.ml
+++ 8.ml
@@ -26,3 +26,19 @@ let samefringe a b =
 let ex1 = Node (Node (Leaf 1, Leaf 2), Leaf 3)
 let ex2 = Node (Leaf 1, Node (Leaf 2, Leaf 3))
 let ex3 = Node (Node (Leaf 1, Leaf 3), Leaf 2)
+
+(* ------------------ *)
+
+type regexp = 
+    | Atom of char
+    | And of regexp * regexp
+    | Or of regexp * regexp
+    | Star of regexp
+
+let rec match_regexp r str next fail = match r with
+    | Atom c     -> if str = [] || List.hd str <> c then fail() else next (List.tl str) fail
+    | And (a, b) -> match_regexp a str (fun str' fail' -> match_regexp b str' next fail') fail
+    | Or (a, b)  -> match_regexp a str next (fun () -> match_regexp b str next fail)
+    | Star r     -> next str (fun () -> match_regexp (And (r, Star r)) str next fail)
+
+let run r str = match_regexp r str (fun str fail -> if str = [] then true else fail()) (fun () -> false)
