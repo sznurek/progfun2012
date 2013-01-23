3ead74a83aa7806896395287e6c0cac7d62202ed
Łukasz Dąbek <sznurek@gmail.com>
2013-01-24 00:43:27 +0100

    Making funny indentation in Racket.

--- 10.rkt
+++ 10.rkt
@@ -47,28 +47,63 @@
 
 (define (run S E C D)
   (cond
-    [(and (eq? (length S) 1) (empty? C) (empty? D)) (first S)]
-    [(and (eq? (length S) 1) (empty? C) (not (empty? D))) 
-         (let ([s (car (first D))]
-               [e (cadr (first D))]
-               [c (caddr (first D))])
-           (run (append S s) e c (rest D)))]
-    [(and (not (empty? C)) (integer? (first C))) (run (cons (first C) S) E (rest C) D)]
-    [(and (not (empty? C)) (string? (first C))) (run (cons (env-lookup E (first C)) S) E (rest C) D)]
-    [(and (not (empty? C)) (lambda? (first C))) (run (cons (make-closure E (first C)) S) E (rest C) D)]
-    [(and (not (empty? C)) (app? (first C))) (run S E (append (list (app-argument (first C)) (app-lambda (first C)) 'apply) (rest C)) D)]
-    [(and (>= (length S) 2) (eq? (first S) 'succ) (integer? (first (rest S))) (not (empty? C)) (eq? (first C) 'apply))
-         (let ([int (first (rest S))]
-               [Sp (rest (rest S))])
-           (run (cons (+ int 1) Sp) E C D))]
-    [(and (not (empty? C)) (>= (length S) 2) (closure? (first S)) (eq? (first C) 'apply))
-         (let* ([c (first S)]
-                [v (first (rest S))]
-                [Ep (closure-env c)]
-                [x (lambda-var (closure-lambda c))]
-                [t (lambda-body (closure-lambda c))])
-           (run '() (env-extend Ep x v) (list t) (cons (list (rest (rest S)) E (rest C)) D)))]
+    [(and
+       (eq? (length S) 1)
+       (empty? C)
+       (empty? D))
+     (first S)]
+    [(and
+       (eq? (length S) 1)
+       (empty? C)
+       (not (empty? D)))
+     (let ([s (car (first D))]
+           [e (cadr (first D))]
+           [c (caddr (first D))])
+       (run (append S s) e c (rest D)))]
+    [(and
+       (not (empty? C))
+       (integer? (first C)))
+     (run (cons (first C) S) E (rest C) D)]
+    [(and
+       (not (empty? C))
+       (string? (first C)))
+     (run (cons (env-lookup E (first C)) S) E (rest C) D)]
+    [(and
+       (not (empty? C))
+       (lambda? (first C)))
+     (run (cons (make-closure E (first C)) S) E (rest C) D)]
+    [(and
+       (not (empty? C))
+       (app? (first C)))
+     (run S E (append (list (app-argument (first C)) (app-lambda (first C)) 'apply) (rest C)) D)]
+    [(and
+       (>= (length S) 2)
+       (eq? (first S) 'succ)
+       (integer? (first (rest S)))
+       (not (empty? C))
+       (eq? (first C) 'apply))
+     (let ([int (first (rest S))]
+           [Sp (rest (rest S))])
+       (run (cons (+ int 1) Sp) E C D))]
+    [(and
+       (not (empty? C))
+       (>= (length S) 2)
+       (closure? (first S))
+       (eq? (first C) 'apply))
+     (let* ([c (first S)]
+            [v (first (rest S))]
+            [Ep (closure-env c)]
+            [x (lambda-var (closure-lambda c))]
+            [t (lambda-body (closure-lambda c))])
+       (run '() (env-extend Ep x v) (list t) (cons (list (rest (rest S)) E (rest C)) D)))]
     [#t (cons 'malformed-state (list S E C D))]))
 
 (define (evaluate t)
   (run '() (env-extend env-empty "succ" 'succ) (list t) '()))
+
+(define (const n)
+  (make-lambda "f" (make-lambda "x" n)))
+
+(define fixpoint
+  (make-lambda "f" (make-lambda "x" (make-app (make-app "f" "f") "x"))))
+
